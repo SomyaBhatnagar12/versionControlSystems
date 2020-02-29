5ecdfdbb9b226d73e1b3029fa2250a8b22edfc1e
SomyaBhatnagar12 <sbhatnagar@student.nitw.ac.in>
2020-02-29 14:51:47 +0530

    created sub.c

---
+++ sub.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a, b, c;
+   
+   cout << "Enter two integers to subtract\n";
+   cin >> a >> b;
+
+   c = a - b;
+   cout <<"diff of the numbers: " << c << endl;
+   
+   return 0;
+}
